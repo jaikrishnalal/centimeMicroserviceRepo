f08cfd0e34e9c6609f5bc005bc996e1441913688
JAI KISHAN LAL <jklife01@gmail.com>
2022-08-29 08:33:09 +0530

    adding swagger

--- swagger-microservice/src/main/java/com/personal/application/swaggermicroservice/SwaggerMicroserviceApplication.java
+++ swagger-microservice/src/main/java/com/personal/application/swaggermicroservice/SwaggerMicroserviceApplication.java
@@ -10,6 +10,9 @@ import org.springframework.web.client.RestTemplate;
 
 import com.personal.application.swaggermicroservice.exception.ServiceExceptionHandler;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -23,6 +26,11 @@ public class SwaggerMicroserviceApplication {
 		return new RestTemplate();
 	}
 	
+	@Bean
+    public Docket productApi() {
+	      return new Docket(DocumentationType.SWAGGER_2).select()
+	         .apis(RequestHandlerSelectors.basePackage("com.personal.application.swaggermicroservice")).build();
+	   }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerMicroserviceApplication.class, args);
--- userservice/src/main/java/com/personal/application3/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/personal/application3/userservice/UserserviceApplication.java
@@ -4,7 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,6 +16,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class UserserviceApplication {
 	private static Logger log = LoggerFactory.getLogger(UserserviceApplication.class);
 
+	@Bean
+    public Docket productApi() {
+	      return new Docket(DocumentationType.SWAGGER_2).select()
+	         .apis(RequestHandlerSelectors.basePackage("com.personal.application3.userservice")).build();
+	   }
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
