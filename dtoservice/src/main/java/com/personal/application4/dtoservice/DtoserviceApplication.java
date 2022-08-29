44dc7de38744aa2e851d2eab04161868ac5f8eda
JAI KISHAN LAL <jklife01@gmail.com>
2022-08-29 08:58:57 +0530

    dto commit initial

---
+++ dtoservice/src/main/java/com/personal/application4/dtoservice/DtoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.personal.application4.dtoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtoserviceApplication.class, args);
+	}
+
+}
---
+++ dtoservice/src/test/java/com/personal/application4/dtoservice/DtoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.application4.dtoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
